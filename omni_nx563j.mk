abb06eddeb9fa8c261e8a83bf66718c40e5ca802
RuRuTiaSaMa <1009087450@qq.com>
2019-01-27 18:00:29 +0800

    Initial device tree for TWRP for android pie
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

---
+++ omni_nx563j.mk
@@ -0,0 +1,42 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := nx563j
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := nx563j
+PRODUCT_NAME := omni_nx563j
+PRODUCT_BRAND := nubia
+PRODUCT_MODEL := Z17
+PRODUCT_MANUFACTURER := nubia
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_GMS_CLIENTID_BASE := android-zte
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=NX563J PRODUCT_NAME=NX563J
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="NX563J-user 8.1.0 OPM1.171019.011 eng.nubia.20181115.164053 release-keys"
+
+BUILD_FINGERPRINT := nubia/NX563J/NX563J:8.1.0/OPM1.171019.011/eng.nubia.20181115.164053:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo omni_nx563j-eng
+add_lunch_combo omni_nx563j-userdebug
